1b38189826dbb56b035b9362f526b26ea7c7c54f
oTalDoMagalas <gustavomagalas.senai@gmail.com>
2025-02-18 16:36:02 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+const clickesq = () => {
+  console.log("Clicado com sucesso esquerda");
+};
+
+const clickdir = () => {
+  console.log("Clicado com sucesso direita");
+};
+*/
+
+let lista = document.querySelectorAll('.item');
+let next = document.getElementById('proximo');
+let prev = document.getElementById('anterior');
+let contar = lista.length;
+let ativo = 0;
+
+function proximoSlide() {
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    
+    if (ativo >= contar - 1) {
+        ativo = 0;
+    } else {
+        ativo += 1;
+    }
+    
+    lista[ativo].classList.add('ativo');
+}
+
+function anteriorSlide() {
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    
+    if (ativo <= 0) {
+        ativo = contar - 1;
+    } else {
+        ativo -= 1;
+    }
+    
+    lista[ativo].classList.add('ativo');
+}
+
+next.onclick = proximoSlide;
+prev.onclick = anteriorSlide;
+
+// Faz a troca automática a cada 3 segundos
+setInterval(proximoSlide, 3000);
